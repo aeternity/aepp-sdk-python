76ae1ee7fc35608a5ff90a82a45aee6cc8d1b52c
Tom Wallroth <tomwallroth@gmail.com>
2018-02-15 19:27:18 +0100

    run tests for both EVM and RING

--- aeternity/tests/test_contract.py
+++ aeternity/tests/test_contract.py
@@ -1,4 +1,3 @@
-from aeternity import EpochClient
 from aeternity.contract import Contract
 
 # see: /epoch/apps/aering/test/contracts/identity.aer
@@ -16,20 +15,46 @@ contract Identity = {
 };
 '''
 
-def test_contract_compile():
+#
+# RING
+#
+
+def test_ring_contract_compile():
     contract = Contract(example_contract, Contract.RING)
     result = contract.compile('')
     assert result is not None
     assert result.startswith('0x')
 
-def test_contract_call():
+def test_ring_contract_call():
     contract = Contract(example_contract, Contract.RING)
     result = contract.call('Identity.main', '1')
     assert result is not None
     assert result.get('out')
 
-def test_encode_calldata():
+def test_ring_encode_calldata():
     contract = Contract(example_contract, Contract.RING)
+    result = contract.encode_calldata('Identity.main', '1')
+    assert result is not None
+    assert result == 'Identity.main1'
+
+#
+# EVM
+#
+
+def test_evm_contract_compile():
+    contract = Contract(example_contract, Contract.EVM)
+    result = contract.compile('')
+    assert result is not None
+    assert result.startswith('0x')
+
+def test_evm_contract_call():
+    contract = Contract(example_contract, Contract.EVM)
+    result = contract.call('Identity.main', '1')
+    assert result is not None
+    assert result.get('out')
+
+def test_evm_encode_calldata():
+    contract = Contract(example_contract, Contract.EVM)
     result = contract.encode_calldata('Identity.main', '1')
     assert result is not None
     assert result == 'Identity.main1'
