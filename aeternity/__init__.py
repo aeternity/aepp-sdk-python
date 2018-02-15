3d4770bbcc03ec4aa77495b3b6c0aedc01288951
Tom Wallroth <tomwallroth@gmail.com>
2018-02-15 18:44:45 +0100

    renamed `get_current_height` to `get_height`, let `Contract` object contain the code
    added aevm helper class that allows for printing pretty bytecode of the compiled contract

--- aeternity/__init__.py
+++ aeternity/__init__.py
@@ -2,5 +2,6 @@ from .oracle import Oracle, OracleQuery
 from .aens import AEName
 from .epoch import EpochClient
 from .config import Config
+from .contract import Contract
 
 __version__ = '0.1.0'
--- aeternity/tests/test_contract.py
+++ aeternity/tests/test_contract.py
@@ -3,50 +3,35 @@ from aeternity.contract import Contract
 
 client = EpochClient()
 
-# see: /epoch/apps/aering/test/contracts/05_greeter.aer
+# see: /epoch/apps/aering/test/contracts/identity.aer
 example_contract = '''
-contract type Counter = {
+contract type Identity = {
   type state;
-  let init : unit => state;
-  let get  : unit => int;
-  let tick : unit => unit;
+
+  let main: int => int;
 };
 
-contract Counter = {
-
-  type state = int;
-
-  let init() = 0;
-
-  let get() = {
-    let s = state();
-    (s, s)
-  };
-
-  let tick() = {
-    let s = state();
-    ((), s + 1)
+contract Identity = {
+  let main(x:int) = {
+    x;
   };
 };
-
-
 '''
 
 def test_contract_compile():
-    contract = Contract(client)
-    result = contract.contract_compile(example_contract, '')
+    contract = Contract(example_contract, client)
+    result = contract.compile('')
     assert result is not None
-    print(result)
-    assert result.get('bytecode')
+    assert result.startswith('0x')
 
 def test_contract_call():
-    contract = Contract(client)
-    result = contract.call(example_contract, 'init', '0x9')
+    contract = Contract(example_contract, client)
+    result = contract.call('Identity.main', '1')
     assert result is not None
     assert result.get('out')
 
 def test_encode_calldata():
-    contract = Contract(client)
-    result = contract.encode_calldata(example_contract, 'init', '1')
+    contract = Contract(example_contract, client)
+    result = contract.encode_calldata('Identity.main', '1')
     assert result is not None
     assert result.startswith('0x')
