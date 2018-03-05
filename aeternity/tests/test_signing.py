e5aa572164943f7f7c02ac6864625b64ca06380c
Tom Wallroth <tomwallroth@gmail.com>
2018-03-05 13:51:44 +0000

    got offline transaction signing to work

--- aeternity/tests/test_signing.py
+++ aeternity/tests/test_signing.py
@@ -1,24 +1,15 @@
 from aeternity import EpochClient
-from aeternity.signing import base58encode, base58decode, KeyPair
-
-
-def test_b58_encode():
-    assert base58encode(b'@a') == '5uA'
-
-def test_b58_decode():
-    assert base58decode('5uA') == b'@a'
-
-def test_b58_roundtrip():
-    assert base58decode(base58encode(b'KAPOW')) == b'KAPOW'
+from aeternity.signing import KeyPair
 
 def test_create_transaction_signing():
     client = EpochClient()
-    transaction = client.create_transaction(client.get_pubkey(), 10)
+
+    new_keypair = KeyPair.generate()
+    receiver_address = new_keypair.get_address()
+
     keypair = KeyPair.read_from_dir('/home/tom/data/aeternity/epoch/_build/dev1/rel/epoch/data/aecore/keys/', 'secret')
+
+    transaction = client.create_transaction(receiver_address, 10)
     signed_transaction = keypair.sign_transaction(transaction)
-    print('signed transaction')
-    print(signed_transaction)
     result = client.send_signed_transaction(signed_transaction)
-    print('sending signed transaction')
-    print(result)
-    assert False
+    assert result == {}
