accc1041036be932793eed714094f7e798a374b4
Tom Wallroth <tomwallroth@gmail.com>
2018-03-08 11:28:48 +0000

    fixed contract tests using aer-contract language

--- aeternity/tests/test_contract.py
+++ aeternity/tests/test_contract.py
@@ -6,32 +6,20 @@ from aeternity.contract import Contract
 # see: /epoch/apps/aering/test/contracts/identity.aer
 from aeternity.exceptions import AException
 
-example_contract = '''
-contract type Identity = {
-  type state;
+aer_identity_contract = '''
 
-  let main: int => int;
-};
+contract Identity =
+  type state = ()
+  function main(x : int) = x
 
-contract Identity = {
-  let main(x:int) = {
-    x;
-  };
-};
 '''
 
 broken_contract = '''
-contract type IdentityBroken = {
-  BROKEN state;
 
-  let main: int => int;
-};
+contract Identity =
+  BROKEN state = ()
+  function main(x : int) = x
 
-contract Identity = {
-  let main(x:int) = {
-    x;
-  };
-};
 '''
 
 #
@@ -39,19 +27,19 @@ contract Identity = {
 #
 
 def test_ring_contract_compile():
-    contract = Contract(example_contract, Contract.RING)
+    contract = Contract(aer_identity_contract, Contract.RING)
     result = contract.compile('')
     assert result is not None
     assert result.startswith('0x')
 
 def test_ring_contract_call():
-    contract = Contract(example_contract, Contract.RING)
+    contract = Contract(aer_identity_contract, Contract.RING)
     result = contract.call('main', '1')
     assert result is not None
     assert result.get('out')
 
 def test_ring_encode_calldata():
-    contract = Contract(example_contract, Contract.RING)
+    contract = Contract(aer_identity_contract, Contract.RING)
     result = contract.encode_calldata('main', '1')
     assert result is not None
     assert result == 'main1'
@@ -79,21 +67,21 @@ def test_ring_broken_encode_calldata():
 #
 
 def test_evm_contract_compile():
-    contract = Contract(example_contract, Contract.EVM)
-    result = contract.compile('')
+    contract = Contract(aer_identity_contract, Contract.EVM)
+    result = contract.compile()
     assert result is not None
     assert result.startswith('0x')
 
 #TODO This call fails with an out of gas exception
 @pytest.mark.skip('This call fails with an out of gas exception')
 def test_evm_contract_call():
-    contract = Contract(example_contract, Contract.EVM)
+    contract = Contract(aer_identity_contract, Contract.EVM)
     result = contract.call('main', '1')
     assert result is not None
     assert result.get('out')
 
 def test_evm_encode_calldata():
-    contract = Contract(example_contract, Contract.EVM)
+    contract = Contract(aer_identity_contract, Contract.EVM)
     result = contract.encode_calldata('main', '1')
     assert result is not None
     assert result == 'main1'
