7cf4f2e050048cdc7c9d92d001c564b888e7f4b2
Tom Wallroth <tomwallroth@gmail.com>
2018-03-13 16:09:31 +0000

    added formatter methods to print the Transactions and blocks nicely

---
+++ aeternity/formatter.py
@@ -0,0 +1,23 @@
+from aeternity.epoch import BlockWithTx, Transaction, AENSClaimTx, AENSPreclaimTx
+
+
+def pretty_account(account_hash):
+    return account_hash[:8] + '...' + account_hash[-6:]
+
+
+def pretty_transaction(tx:Transaction):
+    info = ''
+    if type(tx.tx) == AENSClaimTx:
+        account = pretty_account(tx.tx.account)
+        info = f'name: {tx.tx.name} account: {account}'
+    elif type(tx.tx) == AENSPreclaimTx:
+        account = pretty_account(tx.tx.account)
+        info = f'commitment: {tx.tx.commitment} account: {account}'
+    return f'TX: {tx.tx.type} {info} {tx.hash}'
+
+
+def pretty_block(block:BlockWithTx):
+    transactions = '\n    '.join(pretty_transaction(tx) for tx in block.transactions)
+    return f'''BLOCK: height {block.height} hash: {block.hash}
+transactions:
+    {transactions}'''
