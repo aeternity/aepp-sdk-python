d3d0bafb63f9dddcb2d86b0c73254b7f6656b977
Andrea Giacobino <no.andrea@gmail.com>
2018-07-24 16:59:17 +0200

    Update version to 0.18.0.1 compatible with epoch 0.18.0

--- aeternity/tests/__init__.py
+++ aeternity/tests/__init__.py
@@ -11,7 +11,7 @@ PUBLIC_KEY = os.environ.get('WALLET_PUB')
 PRIVATE_KEY = os.environ.get('WALLET_PRIV')
 NODE_URL = os.environ.get('TEST_URL')
 NODE_URL_INTERNAL = os.environ.get('TEST_INTERNAL_URL')
-EPOCH_VERSION = '0.16.0'
+EPOCH_VERSION = '0.18.0'
 # set the key folder as environment variables
 KEYPAIR = KeyPair.from_public_private_key_strings(PUBLIC_KEY, PRIVATE_KEY)
 
