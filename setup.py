b2a7a1c57a85305f408ff259917d9b0f0a76a8e5
Andrea Giacobino <no.andrea@gmail.com>
2019-02-10 22:43:29 +0100

    Set minimum required python version to 3.6

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     author='Andrea Giacobino',
     author_email='aepp-dev@aeternity.com',
     url='https://github.com/aeternity/aepp-sdk-python',
-    python_requires='>=3.5',
+    python_requires='>=3.6',
     license='ISC',
     scripts=['aecli'],
     packages=find_packages(),
