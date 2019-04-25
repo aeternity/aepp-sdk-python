2f98467c780c2e9831436f5451adbf4988aa10ca
Shubhendu Shekhar <shubh.shekhar1991@gmail.com>
2019-04-25 15:19:52 +0530

    fix(state-channels): updated to more stable websockets library

--- tests/test_channels.py
+++ tests/test_channels.py
@@ -1,19 +1,34 @@
 from aeternity.channel import Channel
+from aeternity import transactions, signing, node, identifiers
+import time
+import asyncio
+import pytest
 
-def test_channel_connection():
+
+@pytest.mark.asyncio
+async def test_channel_connection():
+    acc = signing.Account.from_keystore('/Users/shubhendu/dev/aepp-cli-js/two', '')
+    tx_signer = transactions.TxSigner(
+        acc,
+        'ae_devnet'
+    )
     opts = {
         'url': 'ws://localhost:3014',
         'role': 'initiator',
-        'push_amount': 3,
-        'initiator_amount': 10,
-        'responder_amount': 10,
-        'channel_reserve': 2,
+        'push_amount': 300,
+        'initiator_amount': 100000000000000000,
+        'responder_amount': 100000000000000000,
+        'channel_reserve': 2000,
         'ttl': 10000,
         'host': 'localhost',
         'port': 3001,
-        'lock_period': 100,
-        'initiator_id': 'ak_8wWs1j2vhgjexQmKfgEBrG8ysAucRJdb3jsag3PJKjEeXswb7',
-        'responder_id': 'ak_bmtGbfP3SdPoJNZCQGjjzbKRje15J9CEcWYaL1gZyv2qEyiMe'
+        'lock_period': 1000000,
+        'protocol': 'json-rpc',
+        'initiator_id': 'ak_UHssTe2mXhj6LrbH6MfGQNbvn6X8YAgwjwdgefEaEB1d2HdLX',
+        'responder_id': 'ak_2jgyqAtNS68QWgCej56sZpGpRrEwvUyPpL5ZbG4TeZBxByBMS8',
+        'sign': tx_signer.sign_encode_transaction
     }
-    Channel(opts)
+    channel = Channel(opts)
+    await channel.create()
+    
 
