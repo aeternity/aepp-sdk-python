bd142de82c232e359891e6d31da4e90f92498145
shekhar-shubhendu <shubh.shekhar1991@gmail.com>
2019-05-17 11:14:47 +0200

    fix(state-channels): refactor code. remove __sign_shutdown method

--- aeternity/channel.py
+++ aeternity/channel.py
@@ -106,9 +106,9 @@ class Channel(object):
                 if msg.params.channel_id is not None:
                     self.id = msg.params.channel_id
             if msg.method == f"channels.sign.{self.params.role}_sign":
-                self.__sign_channel_tx(msg.params.data.tx)
+                self.__sign_channel_tx(f'channels.{self.params.role}_sign', msg.params.data.tx)
             if msg.method == "channels.sign.shutdown_sign" or msg.method == "channels.sign.shutdown_sign_ack":
-                self.__sign_shutdown(msg.method.replace("sign.", ""), msg.params.data.tx)
+                self.__sign_channel_tx(msg.method.replace("sign.", ""), msg.params.data.tx)
 
     def __channel_url(self, url, params, endpoint):
         """
@@ -147,13 +147,13 @@ class Channel(object):
         accounts = accounts if accounts else [self.params.initiator_id, self.params.responder_id]
         self.__channel_call('channels.get.balances', {'accounts': accounts})
 
-    def __sign_channel_tx(self, tx):
+    def __sign_channel_tx(self, method, tx):
         """
         Sign the transactions received over channel by the provided sign method
         """
         signedTx = self.sign(tx)
         self.__enqueue_action({
-            'method': f'channels.{self.params.role}_sign',
+            'method': method,
             'params': {
                 'tx': signedTx.tx
             }
@@ -228,15 +228,6 @@ class Channel(object):
             'method': 'channels.withdraw',
             'params': {
                 'amount': amount
-            }
-        })
-
-    def __sign_shutdown(self, method, tx):
-        signedTx = self.sign(tx)
-        self.__enqueue_action({
-            'method': method,
-            'params': {
-                'tx': signedTx.tx
             }
         })
 
