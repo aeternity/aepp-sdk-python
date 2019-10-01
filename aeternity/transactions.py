ea5fce1bf4dd5fe519b55ba0940a6bda1edd5cae
Shubhendu Shekhar <shubh.shekhar1991@gmail.com>
2019-10-01 12:41:33 +0200

    add tx_spend_name

--- aeternity/transactions.py
+++ aeternity/transactions.py
@@ -909,6 +909,7 @@ class TxBuilder:
         :param ttl: the absolute ttl of the transaction
         :param nonce: the nonce of the transaction
         """
+
         name_id = namehash_encode(idf.NAME_ID, recipient_name)
         print("Name ID: ", name_id)
         body = dict(
