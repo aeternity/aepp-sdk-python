af63cd95826c558b2b66d90bd0ae10e5a93aaea5
shekhar-shubhendu <shubh.shekhar1991@gmail.com>
2019-10-23 16:42:27 +0200

    feat(contracts): add bindings param

--- aeternity/contract_native.py
+++ aeternity/contract_native.py
@@ -66,7 +66,7 @@ class ContractNative(object):
         def contract_method(*args, **kwargs):
             tansformed_args = []
             for i, val in enumerate(args):
-                tansformed_args.append(self.sophia_transformer.convert_to_sophia(val, namedtupled.reduce(method.typeDef[i].type)))
+                tansformed_args.append(self.sophia_transformer.convert_to_sophia(val, namedtupled.reduce(method.typeDef[i].type), self.aci.encoded_aci))
             calldata = self.compiler.encode_calldata(self.source, method.name, *tansformed_args).calldata
             call_tx = self.call(method.name, calldata)
             call_info = self.contract.get_call_object(call_tx.hash)
@@ -165,8 +165,8 @@ class SophiaTransformation:
         if isinstance(t, str):
             return t, None
 
-    def convert_to_sophia(self, argument, sophia_type):
-        current_type, generic = self.extractType(sophia_type)
+    def convert_to_sophia(self, argument, sophia_type, bindings={}):
+        current_type, generic = self.extractType(sophia_type, bindings)
         method_name = self.TO_SOPHIA_METHOD_PREFIX + current_type
         method = getattr(self, method_name, None)
         if method is None:
