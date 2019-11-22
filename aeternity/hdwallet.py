364ef7191ff43e36c636c1aa883f1dc447db2afa
Shubhendu Shekhar <shubh.shekhar1991@gmail.com>
2019-11-22 11:42:29 +0100

    update hd wallet and test

--- aeternity/hdwallet.py
+++ aeternity/hdwallet.py
@@ -4,95 +4,107 @@ from nacl.signing import SigningKey
 from mnemonic import Mnemonic
 from aeternity.signing import Account
 
-HARDENED_OFFSET = 0x800001c9
-AETERNITY_DERIVATION_PATH = "m/44'/457'/0'"
-MNEMONIC = Mnemonic(language="english")
 
+class HDWallet():
 
-def generate_mnemonic():
-    """
-    Generate mnemonic
-    """
-    return MNEMONIC.generate()
+    HARDENED_OFFSET = 0x800001c9
+    AETERNITY_DERIVATION_PATH = "m/44'/457'/%d'/0'/%d'"
+    MNEMONIC = Mnemonic(language="english")
 
+    def __init__(self, mnemonic=None):
+        if mnemonic is None:
+            mnemonic = self.generate_mnemonic()
+        if not self.MNEMONIC.check(mnemonic):
+            raise ValueError("Invalid Mnemonic")
+        self.mnemonic = mnemonic
+        self.master_key = self._master_key_from_mnemonic(mnemonic)
+        self.master_account = self._get_account(self.master_key["secret_key"])
+        self.account_index = 0
 
-def generate_wallet(mnemonic=None):
-    """
-    Generate Account/Wallet
-    """
-    if mnemonic is None:
-        mnemonic = generate_mnemonic()
-    if not MNEMONIC.check(mnemonic):
-        raise ValueError("Invalid Mnemonic")
-    keys = _master_key_from_mnemonic(mnemonic)
-    return mnemonic, _get_account(keys["secret_key"])
+    @staticmethod
+    def generate_mnemonic():
+        """
+        Generate mnemonic
+        """
+        return HDWallet.MNEMONIC.generate()
 
+    def derive_child(self, account_index=None, address_index=0):
+        if account_index is None:
+            account_index = self.account_index
+            self.account_index += 1
+        derivation_path = self.AETERNITY_DERIVATION_PATH % (account_index, address_index)
+        derived_keys = self._from_path(derivation_path, self.master_key)
+        return derivation_path, self._get_account(derived_keys[-1]["secret_key"])
 
-def _get_account(secret_key):
-    """
-    Generate Account from secret key
+    def get_master_key(self):
+        return self.master_key
 
-    Args:
-        secret_key(bytes): secret key in bytes
-    Returns:
-        Account instance with generated signing key and verifying key
-    """
-    sign_key = SigningKey(secret_key)
-    return Account(sign_key, sign_key.verify_key)
+    def get_master_account(self):
+        return self.master_account
 
+    def _get_account(self, secret_key):
+        """
+        Generate Account from secret key
 
-def _master_key_from_mnemonic(mnemonic):
-    return _master_key_from_seed(Mnemonic.to_seed(mnemonic))
+        Args:
+            secret_key(bytes): secret key in bytes
+        Returns:
+            Account instance with generated signing key and verifying key
+        """
+        sign_key = SigningKey(secret_key)
+        return Account(sign_key, sign_key.verify_key)
 
+    @staticmethod
+    def _master_key_from_mnemonic(mnemonic):
+        return HDWallet._master_key_from_seed(Mnemonic.to_seed(mnemonic))
 
-def _master_key_from_seed(seed):
-    """
-    Generates a master key from a provided seed.
+    @staticmethod
+    def _master_key_from_seed(seed):
+        """
+        Generates a master key from a provided seed.
 
-    Args:
-        seed (str or bytes): a string of bytes or a hex string
-    Returns:
-        dict containing 'secret_key' and 'chain_code'
-    """
-    seed = bytes.fromhex(seed) if type(seed) is str else seed
-    I_hmac = hmac.new(b'ed25519 seed', seed, hashlib.sha512).digest()
-    return {"secret_key": I_hmac[:32], "chain_code": I_hmac[32:]}
+        Args:
+            seed (str or bytes): a string of bytes or a hex string
+        Returns:
+            dict containing 'secret_key' and 'chain_code'
+        """
+        seed = bytes.fromhex(seed) if type(seed) is str else seed
+        I_hmac = hmac.new(b'ed25519 seed', seed, hashlib.sha512).digest()
+        return {"secret_key": I_hmac[:32], "chain_code": I_hmac[32:]}
 
-
-def _parse_path(path):
-    if isinstance(path, str):
-        p = path.rstrip("/").split("/")
-    elif isinstance(path, bytes):
-        p = path.decode('utf-8').rstrip("/").split("/")
-    else:
-        p = list(path)
-
-    return p
-
-
-def _derive_child(parent_key, i):
-    if i & HARDENED_OFFSET:
-        hmac_data = b'\x00' + parent_key["secret_key"] + i.to_bytes(length=4, byteorder='big')
-    I_hmac = hmac.new(parent_key["chain_code"], hmac_data, hashlib.sha512).digest()
-    return {"secret_key": I_hmac[:32], "chain_code": I_hmac[32:]}
-
-
-def _from_path(path, root_key, is_master=True):
-    p = _parse_path(path)
-
-    if p[0] == "m":
-        if is_master:
-            p = p[1:]
+    @staticmethod
+    def _parse_path(path):
+        if isinstance(path, str):
+            p = path.rstrip("/").split("/")
+        elif isinstance(path, bytes):
+            p = path.decode('utf-8').rstrip("/").split("/")
         else:
-            raise ValueError("root_key must be a master key if 'm' is the first element of the path.")
+            p = list(path)
 
-    keys = [root_key]
-    for i in p:
-        if isinstance(i, str):
-            index = HARDENED_OFFSET + int(i[:-1])
-        else:
-            index = i
-        k = keys[-1]
-        keys.append(_derive_child(k, index))
+        return p
 
-    return keys
+    def _derive_child_key(self, parent_key, i):
+        if i & self.HARDENED_OFFSET:
+            hmac_data = b'\x00' + parent_key["secret_key"] + i.to_bytes(length=4, byteorder='big')
+        I_hmac = hmac.new(parent_key["chain_code"], hmac_data, hashlib.sha512).digest()
+        return {"secret_key": I_hmac[:32], "chain_code": I_hmac[32:]}
+
+    def _from_path(self, path, root_key, is_master=True):
+        p = self._parse_path(path)
+
+        if p[0] == "m":
+            if is_master:
+                p = p[1:]
+            else:
+                raise ValueError("root_key must be a master key if 'm' is the first element of the path.")
+
+        keys = [root_key]
+        for i in p:
+            if isinstance(i, str):
+                index = self.HARDENED_OFFSET + int(i[:-1])
+            else:
+                index = i
+            k = keys[-1]
+            keys.append(self._derive_child_key(k, index))
+
+        return keys
--- tests/test_hd_wallet.py
+++ tests/test_hd_wallet.py
@@ -1,10 +1,34 @@
-from aeternity.hdwallet import generate_mnemonic, generate_wallet
+from aeternity.hdwallet import HDWallet
 
 def test_hdwallet_generate_wallet():
-    mnemonic = generate_mnemonic()
-    _, account = generate_wallet(mnemonic)
-    assert account is not None
+    mnemonic = HDWallet.generate_mnemonic()
+    assert mnemonic is not None
+    hdwallet = HDWallet(mnemonic)
+    assert hdwallet.master_key is not None
 
 def test_hdwallet_generate_wallet_without_mnemonic():
-    _, account = generate_wallet()
-    assert account is not None
+    hdwallet = HDWallet()
+    assert hdwallet.mnemonic is not None
+
+def test_hdwallet_path_derivation():
+    mnemonic = "energy pass install genuine sell enroll wear announce brother marble test cruise"
+    hdwallet = HDWallet(mnemonic)
+
+    expected_path = "m/44'/457'/0'/0'/0'"
+    expected_address = "ak_2De9RicLgfnYFWWKqcDiqSEoTetyCrKiBjdJqPDb9ViCzkGRE8"
+    expected_private_key = "7632d27fd7adccdd8a4b6f1e02028ba6ec6262575a533f3dfb06a7cfe711f1d4a065d306530d7d7fa6b01cf2ef73a04d9d2da98700b3c797f55cc36c9fb89ab0"
+
+    path, child_account = hdwallet.derive_child()
+    assert expected_path == path
+    assert child_account.address == expected_address
+    assert child_account.get_secret_key() == expected_private_key
+
+
+    expected_path = "m/44'/457'/12'/0'/0'"
+    expected_address = "ak_2a5KtTDNooCWxTGJuB5PRjhfeHvuvc6CuySXiwjp3SPpYb1ZeM"
+    expected_private_key = "78c4652b9fd2484763f9280b4b3df5802284b2c0bc92a459703ade27ae28d8efcecbc509ae4d242edda112ac10e2d1913c5103d1ff9046d494a08fa6bd12f833"
+
+    path, child_account = hdwallet.derive_child(account_index=12)
+    assert expected_path == path
+    assert child_account.address == expected_address
+    assert child_account.get_secret_key() == expected_private_key
