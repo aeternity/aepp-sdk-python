d96766dffb0f682600ef84d39ee1daf43877dc53
Shubhendu Shekhar <shubh.shekhar1991@gmail.com>
2019-11-25 09:23:31 +0100

    use the bitcoin offset. update tests.

--- tests/test_hd_wallet.py
+++ tests/test_hd_wallet.py
@@ -15,8 +15,8 @@ def test_hdwallet_path_derivation():
     hdwallet = HDWallet(mnemonic)
 
     expected_path = "m/44'/457'/0'/0'/0'"
-    expected_address = "ak_2De9RicLgfnYFWWKqcDiqSEoTetyCrKiBjdJqPDb9ViCzkGRE8"
-    expected_private_key = "7632d27fd7adccdd8a4b6f1e02028ba6ec6262575a533f3dfb06a7cfe711f1d4a065d306530d7d7fa6b01cf2ef73a04d9d2da98700b3c797f55cc36c9fb89ab0"
+    expected_address = "ak_anBY5wEamzK1L1L3f4gZr8RrUMEfgSEmRpAJKTETTETA9k6Kp"
+    expected_private_key = "40a0dc9b6be8ca58ec3b6773a118f8237092d0b5f3d48d4c08a6cfbde258aa3d4cb307d4f30e35ea50ddf29043f2827d8c129cd7c03d981a9b0cc79703160e17"
 
     path, child_account = hdwallet.derive_child()
     assert expected_path == path
@@ -25,10 +25,19 @@ def test_hdwallet_path_derivation():
 
 
     expected_path = "m/44'/457'/12'/0'/0'"
-    expected_address = "ak_2a5KtTDNooCWxTGJuB5PRjhfeHvuvc6CuySXiwjp3SPpYb1ZeM"
-    expected_private_key = "78c4652b9fd2484763f9280b4b3df5802284b2c0bc92a459703ade27ae28d8efcecbc509ae4d242edda112ac10e2d1913c5103d1ff9046d494a08fa6bd12f833"
+    expected_address = "ak_2FZU4teXWLyHWVbvf1ajejzKrcuyXffP4jaoedRDvv579mEjiY"
+    expected_private_key = "032374e3b7c11cb2e5dd62a633d13af7de7f231e721b6f797423978e5e4a7faaa4c17e635de2171a515dc4f049b63957ef8778b4b02533f6791554def9ceda97"
 
     path, child_account = hdwallet.derive_child(account_index=12)
+    assert expected_path == path
+    assert child_account.address == expected_address
+    assert child_account.get_secret_key() == expected_private_key
+
+    expected_path = "m/44'/457'/12'/0'/1'"
+    expected_address = "ak_hPd6XVrLFxBGcKwBbeVA3PWNqE5YJk2NrPuUNp9Lp5q4KHvUX"
+    expected_private_key = "96bab5642835618383a48bb759d4179dbc9a0e9bbd79424c973cdfa694fc5c485bb5e51e9d1d7fcf3bd17cd7ae3af549addf1dd4fe66365c72757796e47d5639"
+
+    path, child_account = hdwallet.derive_child(account_index=12, address_index=1)
     assert expected_path == path
     assert child_account.address == expected_address
     assert child_account.get_secret_key() == expected_private_key
